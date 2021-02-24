63a9baf1c0972b9efa74e93081998255dfd6b06e
Ryan Cavallaro <rcavallaro@kitchenbrains.com>
2021-02-24 14:18:13 -0500

    Alpha Release v0.1
    - Working Light-up Test for IOT(RS485) Meridians

--- MeridianLightUpTest/Form1.Designer.cs
+++ MeridianLightUpTest/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refresh COM Ports";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(29, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Lights On!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(29, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(152, 132);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(206, 235);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Meridian Light-up Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- MeridianLightUpTest/Form1.cs
+++ MeridianLightUpTest/Form1.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+using System.Threading;
+using System.Text.RegularExpressions;
 
 namespace MeridianLightUpTest
 {
@@ -14,6 +15,123 @@ namespace MeridianLightUpTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddPortsToListBox();
+        }
+
+        public void AddPortsToListBox()
+        {
+            listBox1.Items.Clear();
+            var ports = GetAllPorts3();
+            foreach (var item in ports)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+            listBox1.SelectedIndex = 0;
+        }
+
+        public List<string> GetAllPorts()
+        {
+            List<string> allPorts = new List<string>();
+            foreach (string portName in SerialPort.GetPortNames())
+            {
+                allPorts.Add(portName);
+            }
+            return allPorts;
+        }
+
+        public List<string> GetAllPorts2()
+        {
+            var pList = new List<string>();
+            listBox1.Items.Clear();
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = GetAllPorts();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                foreach (string s in portList)
+                {
+                    pList.Add(s);
+                }
+            }
+
+            return pList;
+        }
+
+        public List<string> GetAllPorts3()
+        {
+            var pList = new List<string>();
+            
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%USB Serial Port%'"))
+            {
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+
+                foreach (string port in ports)
+                {
+                    // hacky way of getting COM port names
+                    string substr = port.Substring(port.Length - 7);
+                    string portName = Regex.Replace(substr, @"[^a-zA-Z0-9]", "");                    
+
+                    pList.Add(portName);            
+                }
+            }
+
+            return pList;
+        }
+
+        public List<string> GetAllPorts4()
+        {
+            var pList = new List<string>();
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_USBController WHERE DeviceID like '%'"))
+            {
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["DeviceID"].ToString());
+
+                foreach (string port in ports)
+                {
+                    pList.Add(port);
+                    //if (port.Contains("USB Serial Port"))
+                    //{
+                    //    pList.Add(port.Substring(17).Trim(')'));
+                    //}
+                }
+            }
+
+            return pList;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddPortsToListBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SerialPort port = new SerialPort(listBox1.SelectedItem.ToString(), 57600);
+
+                if (!port.IsOpen)
+                    port.Open();
+                else
+                    return;
+
+                // Enter Diagnostic mode
+                port.Write("M 1 30 1 5000 1 0 1 1234\r\n");
+                Thread.Sleep(150);
+                // Turn on all display/LEDs
+                port.Write("M 1 30 1 5016 0 0 1 1\r\n");
+                port.Close();
+            }
+            catch (Exception)
+            {
+                return;
+            }
         }
     }
 }
